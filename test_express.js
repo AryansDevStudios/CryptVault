f914c3c849738bc34dac531247c4f00745e4b587
Aryan Gupta <aryanisspro@gmail.com>
2026-07-08 01:24:17 +0530

    removed temp files

--- test_express.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/api/download/:uuid', (req, res) => res.send('download'));
-app.get('/api/download-multiple', (req, res) => res.send('multiple'));
-app.listen(3001, () => {
-    fetch('http://localhost:3001/api/download-multiple').then(r=>r.text()).then(console.log).then(()=>process.exit());
-});
